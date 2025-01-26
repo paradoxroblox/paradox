2db35d0b5bbea3dfe3734046dd0a80174210d385
paradox <ritualhubrblx@gmail.com>
2025-01-26 16:37:59 +0300

    Add files via upload

---
+++ games_load/gamelist.lua
@@ -0,0 +1,9 @@
+gamelist = {
+    [16560655288] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/pillarchase2.lua",
+    [6870736769] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/daybreak2.lua",
+    [16116270224] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/dandysworld.lua",
+    [914010731] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/roghoul.lua",
+    [14722542056] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/sonicexethedisaster.lua",
+    [6137321701] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/blair.lua",
+    [18687417158] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/forsaken.lua"
+}
---
+++ games_load/gamenames.lua
@@ -0,0 +1,9 @@
+gamenames = {
+    [16560655288] = "pillar chase 2",
+    [6870736769] = "daybreak 2",
+    [16116270224] = "dandy's world",
+    [914010731] = "ro-ghoul",
+    [14722542056] = "sonic.exe: the disaster",
+    [6137321701] = "blair",
+    [18687417158] = "forsaken"
+}
