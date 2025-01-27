65581cbb0872e348e9f8fbec92fb0ab1ad5ef908
paradox <ritualhubrblx@gmail.com>
2025-01-27 23:31:06 +0300

    Update gamelist.lua

--- games_load/gamelist.lua
+++ games_load/gamelist.lua
@@ -1,9 +1,9 @@
 gamelist = {
-    [16560655288] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/pillarchase2.lua",
-    [6870736769] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/daybreak2.lua",
-    [16116270224] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/dandysworld.lua",
-    [914010731] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/roghoul.lua",
-    [14722542056] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/sonicexethedisaster.lua",
-    [6137321701] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/blair.lua",
-    [18687417158] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/forsaken.lua"
+    [5700746583] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/pillarchase2.lua",
+    [2618786652] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/daybreak2.lua",
+    [5569032992] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/dandysworld.lua",
+    [380704901] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/roghoul.lua",
+    [5075303255] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/sonicexethedisaster.lua",
+    [2239430935] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/blair.lua",
+    [6331902150] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/forsaken.lua"
 }
