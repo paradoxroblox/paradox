17aa93f08ee8841f079b6b71416280005bf119ff
paradox <ritualhubrblx@gmail.com>
2025-02-23 22:02:04 +0300

    Update gamelist_earlyaccess.lua

--- games_load/gamelist_earlyaccess.lua
+++ games_load/gamelist_earlyaccess.lua
@@ -1,1 +1,9 @@
-
+gamelistearlyaccess = {
+    [5700746583] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games_earlyaccess/pillarchase2.lua",
+    [2618786652] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/daybreak2.lua",
+    [5569032992] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/dandysworld.lua",
+    [380704901] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/roghoul.lua",
+    [5075303255] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/sonicexethedisaster.lua",
+    [2239430935] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/blair.lua",
+    [6331902150] = "https://raw.githubusercontent.com/paradoxroblox/paradox/refs/heads/discord.gg/paradoxroblox/games/forsaken.lua"
+}
